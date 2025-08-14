23c951de66183c331daddde68bb02db43eee2b26
Prateekagr3083 <prateekkhaitan38@gmail.com>
2025-08-14 17:27:07 +0000

    Aug 14, 2025, 10:57 PM

--- Login.php
+++ Login.php
@@ -11,22 +11,20 @@
     <?php include 'Navbar/Nave.php'; ?>
     
     <main>
-        <div class="form-container">
-            <form class="form" id="login-form">
-                <p class="title">Login now</p>
-                
-                <label>
-                    <input required="" placeholder="" type="email" class="input">
-                    <span>Email</span>
-                </label> 
-                <label>
-                    <input required="" placeholder="" type="password" class="input">
-                    <span>Password</span>
-                </label>
-                <button class="submit">Login</button>
-                <p class="signin">Don't have an account? <a href="Register.php">Register</a></p>
-            </form>
-        </div>
+        <form class="form">
+            <p class="title">Login</p>
+            
+            <label>
+                <input required="" placeholder=" " type="email" class="input">
+                <span>Email</span>
+            </label>
+            <label>
+                <input required="" placeholder=" " type="password" class="input">
+                <span>Password</span>
+            </label>
+            <button class="submit">Login</button>
+            <p class="signin">Don't have an account? <a href="Register.php">Register</a></p>
+        </form>
     </main>
 </body>
 </html>
--- Register.php
+++ Register.php
@@ -15,11 +15,11 @@
             <p class="title">Register</p>
             
             <label>
-                <input required="" placeholder="" type="text" class="input">
+                <input required="" placeholder=" " type="text" class="input">
                 <span>Firstname</span>
             </label>
             <label>
-                <input required="" placeholder="" type="text" class="input">
+                <input required="" placeholder=" " type="text" class="input">
                 <span>Lastname</span>
             </label>
             <label>
@@ -35,15 +35,15 @@
               <span>Contact Number</span>
             </label>
             <label>
-                <input required="" placeholder="" type="email" class="input">
+                <input required="" placeholder=" " type="email" class="input">
                 <span>Email</span>
             </label>
             <label>
-                <input required="" placeholder="" type="password" class="input">
+                <input required="" placeholder=" " type="password" class="input">
                 <span>Password</span>
             </label>
             <label>
-                <input required="" placeholder="" type="password" class="input">
+                <input required="" placeholder=" " type="password" class="input">
                 <span>Confirm Password</span>
             </label>
             <div class="gender-selection">
