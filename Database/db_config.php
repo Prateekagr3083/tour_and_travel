d8b74305f539f86dc22ef373c398fe5324b7a0c9
Prateekagr3083 <prateekkhaitan38@gmail.com>
2025-08-14 18:47:03 +0000

    Aug 15, 2025, 12:17 AM

--- Database/db_config.php
+++ Database/db_config.php
@@ -1,5 +1,7 @@
+<?php
 // db_config.php
 $db_host = 'localhost'; // Database host
 $db_user = 'root'; // Database username
 $db_pass = ''; // Database password
 $db_name = 'Tour_and_travel'; // Database name
+?>
