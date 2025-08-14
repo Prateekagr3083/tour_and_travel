572bae926cd18a2fddf1bcb5dd29de05880d4afa
Prateekagr3083 <prateekkhaitan38@gmail.com>
2025-08-14 14:49:35 +0000

    Aug 14, 2025, 8:19 PM

--- Register.php
+++ Register.php
@@ -6,46 +6,6 @@
     <title>Register</title>
     <link rel="stylesheet" href="CSS/Login.css">
     <link rel="stylesheet" href="CSS/Nave.css">
-    <style>
-        .gender-selection {
-            margin-top: 15px;
-            padding: 0 5px;
-        }
-        .gender-title {
-            display: block;
-            margin-bottom: 8px;
-            color: #878787;
-            font-size: 0.75rem;
-        }
-        .gender-selection label {
-            display: inline-block;
-            margin-right: 15px;
-            font-size: 0.9rem;
-            color: #333;
-            cursor: pointer;
-        }
-        .gender-selection input[type="radio"] {
-            margin-right: 5px;
-            vertical-align: middle;
-        }
-        .terms-conditions {
-            margin-top: 15px;
-            font-size: 0.9rem;
-            color: #555;
-        }
-        .terms-conditions label {
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-        }
-        .terms-conditions input[type="checkbox"] {
-            margin-right: 8px;
-        }
-        .terms-conditions a {
-            color: royalblue;
-            text-decoration: none;
-        }
-    </style>
 </head>
 <body>
     <?php include 'Navbar/Nave.php'; ?>
